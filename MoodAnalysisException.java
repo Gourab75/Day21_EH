9f9ea88285e81b784eb2d4bfbdf302015389cb5a
Gourab kumar Gupta <32832817+GrvSit@users.noreply.github.com>
2022-08-02 11:48:42 +0530

    Exception Handling

---
+++ MoodAnalysisException.java
@@ -0,0 +1,9 @@
+package com.BL.Exception;
+
+public class MoodAnalysisException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public MoodAnalysisException(String msg) {
+		super(msg);
+	}
+}
